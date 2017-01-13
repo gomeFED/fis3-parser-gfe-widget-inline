9a44dbe25fc77c174f073f3ecb5a9a18b2bd558c
Midday <598535774@qq.com>
2017-01-13 10:02:26 +0800

    创建项目

---
+++ README.md
@@ -0,0 +1,17 @@
+# fis3-parser-gfe-widget-inline
+fis3-parser-gfe-widget-inline
+
+
+## INSTALL
+
+```bash
+npm install [-g] fis3-parser-gfe-widget-inline
+```
+
+## USE
+
+```js
+fis.match('/html/**.{html,ftl}', {
+    parser: fis.plugin('gfe-widget-inline')
+});
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * 获取指定域名下的ssi内容并替换相应ssi标签
+ * @param  {string} content     文件内容
+ * @param  {File}   file        fis 的 File 对象 [fis3/lib/file.js]
+ * @param  {object} settings    插件配置属性
+ * @return {string}             处理后的文件内容
+ */
+module.exports = function(content, file, settings) {
+    
+    return content;
+};
